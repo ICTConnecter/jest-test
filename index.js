9d71f9e14a7b14ef9c75a7540336a024d34ddb8b
ICTConnecter <89728531+ICTConnecter@users.noreply.github.com>
2022-12-01 10:55:44 +0900

    FizzBuss実装

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const add = (a, b) => {
-    return (a + b);
+const fizzBuss = (num) => {
+    if(num % 3 === 0 && num % 5 === 0){
+        return 'FizzBuss'
+    }
+    if(num % 3 === 0){
+        return 'Fizz'
+    }
+    if(num % 5 === 0){
+        return 'Buss'
+    }
+    return num.toString()
 }
 
-module.exports = add
+module.exports = fizzBuss
--- index.test.js
+++ index.test.js
@@ -1,5 +1,51 @@
-const add = require('./index')
+const fizzBuss = require('./index')
 
-test('1と2を渡したとき、3を返す', () => {
-    expect(add(1, 2)).toBe(3);
+test("1を渡すと文字列'1'を返す", ()=>{
+    // 準備(Arrange)
+    const input = 1
+    const expectation = '1'
+    // 実行(Act)
+    const actual = fizzBuss(input)
+    // 検証(Assertion)
+    expect(actual).toBe(expectation)
+})
+
+test("2を渡すと文字列'2'を返す", ()=>{
+    // 準備(Arrange)
+    const input = 2
+    const expectation = '2'
+    // 実行(Act)
+    const actual = fizzBuss(input)
+    // 検証(Assertion)
+    expect(actual).toBe(expectation)
+})
+
+test("3を渡すと文字列'Fizz'を返す", ()=>{
+    // 準備(Arrange)
+    const input = 3
+    const expectation = 'Fizz'
+    // 実行(Act)
+    const actual = fizzBuss(input)
+    // 検証(Assertion)
+    expect(actual).toBe(expectation)
+})
+
+test("5を渡すと文字列'Buzz'を返す", ()=>{
+    // 準備(Arrange)
+    const input = 5
+    const expectation = 'Buss'
+    // 実行(Act)
+    const actual = fizzBuss(input)
+    // 検証(Assertion)
+    expect(actual).toBe(expectation)
+})
+
+test("15を渡すと文字列'FizzBuss'を返す", ()=>{
+    // 準備(Arrange)
+    const input = 15
+    const expectation = 'FizzBuss'
+    // 実行(Act)
+    const actual = fizzBuss(input)
+    // 検証(Assertion)
+    expect(actual).toBe(expectation)
 })
